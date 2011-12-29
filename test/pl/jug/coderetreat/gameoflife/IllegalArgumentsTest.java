01f478d1e39d53944f0e0237777d8262061b1fb3
Marek Defeciński <marekdef@gmail.com>
2011-12-29 23:41:09 +0100

    Changed test so it does not require IllegalArguemtnException on bad cell value Added test that shows that result = -1 is bad :)

--- test/pl/jug/coderetreat/gameoflife/IllegalArgumentsTest.java
+++ test/pl/jug/coderetreat/gameoflife/IllegalArgumentsTest.java
@@ -1,5 +1,7 @@
 package pl.jug.coderetreat.gameoflife;
 
+import junit.framework.Assert;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -57,14 +59,46 @@ public class IllegalArgumentsTest {
 		algo.countAliveNeighbours(null, 0, 0);
 	}
 	
-	@Test(expected = IllegalArgumentException.class)
+	@Test
 	public void testTooBigValueInCell() {
-		int testBoard[][] = new int[][] {
+		int testBoardWithBigValue[][] = new int[][] {
 				{0, 1, 0},
 				{0, 5, 0}
 		};
 		
-		algo.countAliveNeighbours(testBoard, 1, 1);
+		int testBoardWithNormal[][] = new int[][] {
+				{0, 1, 0},
+				{0, 1, 0}
+		};
+				
+		int countAliveNeighboursBigValue = algo.countAliveNeighbours(testBoardWithBigValue, 1, 1);
+		int countAliveNeighboursNormalValue = algo.countAliveNeighbours(testBoardWithNormal, 1, 1);
+		
+		Assert.assertEquals(1, countAliveNeighboursBigValue);
+		Assert.assertEquals(1, countAliveNeighboursNormalValue);
+	}
+	
+	@Test
+	public void testDeadInCenter() {
+		int testBoardOneNeighbour[][] = new int[][] {
+				{1, 0, 0},
+				{0, 0, 0},
+				{0, 0, 0}
+		};
+		
+		int testBoardAllZero[][] = new int[][] {
+				{0, 0, 0},
+				{0, 0, 0},
+				{0, 0, 0}
+		};
+		
+		int countAliveNeighboursOneNeighbour = algo.countAliveNeighbours(testBoardOneNeighbour, 1, 1);
+		
+		int countAliveNeighboursZeroNeighbour = algo.countAliveNeighbours(testBoardAllZero, 1, 1);
+		
+		Assert.assertEquals(1, countAliveNeighboursOneNeighbour);
+		Assert.assertEquals(0, countAliveNeighboursZeroNeighbour);
 	}
 	
 }
+
