992ad3ada29aedb46edac0a4748ce4bef8378acd
Marek Defeciński <marekdef@gmail.com>
2011-12-29 23:28:22 +0100

    Refactored names to row, column Changed indexation in algorithm because it was mixed

--- src/pl/jug/coderetreat/gameoflife/Algorithm.java
+++ src/pl/jug/coderetreat/gameoflife/Algorithm.java
@@ -2,24 +2,24 @@ package pl.jug.coderetreat.gameoflife;
 
 public class Algorithm {
 
-	public int countAliveNeighbours(int[][] testBoard, int i, int j) {
+	public int countAliveNeighbours(int[][] testBoard, int row, int column) {
 		if(testBoard == null) {
 			throw new IllegalArgumentException("Board not initialized");
 		}
 		
-		if(i < 0 || j < 0 || i >= testBoard.length || j >= testBoard[0].length) {
-			throw new IllegalArgumentException(String.format("Coordinates %d %d out of bounds %d %d", i, j, testBoard.length, testBoard[0].length));
+		if(row < 0 || column < 0 || row >= testBoard.length || column >= testBoard[0].length) {
+			throw new IllegalArgumentException(String.format("Coordinates %d %d out of bounds %d %d", row, column, testBoard.length, testBoard[0].length));
 		}
 		
 		int result = -1;
 		
-		int startX = Math.max(0, i - 1);
-		int startY = Math.max(0, j - 1);
-		int endX = Math.min(testBoard.length -1, i + 1);
-		int endY = Math.min(testBoard[0].length - 1, j + 1);
+		int startRow = Math.max(0, row - 1);
+		int startColumn = Math.max(0, column - 1);
+		int endRow = Math.min(testBoard.length -1, row + 1);
+		int endColumn = Math.min(testBoard[0].length - 1, column + 1);
 		
-		for(int r = startY; r <= endY; r++) {
-			for(int c = startX; c <= endX; c++) {
+		for(int c = startColumn; c <= endColumn; c++) {
+			for(int r = startRow; r <= endRow; r++) {
 				result += testBoard[r][c];
 			}
 		}
